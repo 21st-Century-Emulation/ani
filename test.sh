0cd170b1d93d4ded0357fb30b8de2766476870f8
David Tyler <david.tyler@metaswitch.com>
2021-04-23 14:27:18 +0100

    Renamed operands to be consistent between microservices

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ docker run --rm --name ani -d -p 8080:8080 ani
 RESULT=`curl -s --header "Content-Type: application/json" \
   --request POST \
   --data '{"opcode":230,"state":{"a":58,"b":1,"c":0,"d":5,"e":15,"h":10,"l":20,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":1,"stackPointer":2,"cycles":0}}' \
-  http://localhost:8080/api/v1/execute?operand=15`
+  http://localhost:8080/api/v1/execute?operand1=15`
 EXPECTED='{"opcode":230,"state":{"a":10,"b":1,"c":0,"d":5,"e":15,"h":10,"l":20,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":true,"carry":false},"programCounter":1,"stackPointer":2,"cycles":7}}'
 
 docker kill ani
